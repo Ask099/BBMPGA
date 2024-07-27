82a2039dc5a43aa65e550f86bef18c96c62a36ba
Ask099 <19839650437@163.com>
2024-07-27 12:43:10 +0800

    EnvironmentalSelection.m

--- BBMPGA/EnvironmentalSelection.m
+++ BBMPGA/EnvironmentalSelection.m
@@ -10,15 +10,14 @@ function [Population,FrontNo,CrowdDis] = EnvironmentalSelection(P,N,Problem)
         FrontNo_low(:,i) = NDSort(objs(:,1+(temp)*(i-1):temp*i),cons,Nin);
     end
     
-     %  Diff_FrontNo=(max(FrontNo_low,[],2)-min(FrontNo_low,[],2));
-     %    n=max(Diff_FrontNo);
-     %     alpha = 1-0.8*power(Problem.calcount/Problem.maxFE,2);
-     % 
-     %  Diff_FrontNo=Diff_FrontNo>(n*alpha);
-     %  objs=objs+Diff_FrontNo.*max(max(objs))*10;
-     % for i =1:Problem.DM
-     %    FrontNo_low(:,i) = NDSort(objs(:,1+(temp)*(i-1):temp*i),cons,Nin);
-     % end
+    Diff_FrontNo=(max(FrontNo_low,[],2)-min(FrontNo_low,[],2));
+    n=max(Diff_FrontNo);
+    alpha = 1-0.8*power(Problem.calcount/Problem.maxFE,2); 
+    Diff_FrontNo=Diff_FrontNo>(n*alpha);
+    objs=objs+Diff_FrontNo.*max(max(objs))*10;
+    for i =1:Problem.DM
+        FrontNo_low(:,i) = NDSort(objs(:,1+(temp)*(i-1):temp*i),cons,Nin);
+     end
                                                                                     
     [FrontNo,MaxFNo] = NDSort(FrontNo_low,cons,N); 
     Next = FrontNo < MaxFNo;
